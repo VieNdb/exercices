5ee05f93d075848fdf4b2f1e9b4af519af5b0032
Flavie <flaviengbosando10@gmail.com>
2021-03-16 10:42:43 +0100

    algo day 3

---
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/02_use_callback/src/getInput.js
@@ -0,0 +1,8 @@
+// A function that takes two parameters, the second one being a callback function
+
+const getInput = (user, callback) => {
+  callback(user.key);
+};  // code the function here
+
+// Leave the line below for tests to work
+module.exports = getInput;
---
+++ 02_programming/03_algorithms_day_3/85a6324f3dedf296bf6a4fa08629ebe71779a871/04_square_digits/src/squareDigits.js
@@ -0,0 +1,9 @@
+function squareDigits(valueOfNumber, tableOfValue) {
+  // Code here
+for (let i = 0; i < tableOfValue.length; i ++) {
+  return valueOfNumber * valueOfNumber ;
+}
+}
+
+// Leave the line below for tests to work
+module.exports = squareDigits;
