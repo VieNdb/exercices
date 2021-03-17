dea55f15bd25bf8c2f20e4be9191e77188e61c3e
Flavie <flaviengbosando10@gmail.com>
2021-03-17 10:30:09 +0100

    exo js jour 4

--- 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/03_number_game/src/numberGame.js
+++ 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/03_number_game/src/numberGame.js
@@ -1,1 +1,17 @@
-console.log("maman")
+function numberGame(reader, min = 1, max = 100) {
+  // code here
+  const rand = Math.round(Math.random() * (max - min) + min );
+  console.log(`Welcome!
+    You have to find the right number, between 1 and 100!
+    Good luck!!`);
+  reader.question("Enter a number ", value1 => {
+    if (rand === value1) {
+      console.log("You won!");
+    }  else if (rand > value1) {
+      console.log("Too low");
+    } else if (rand < value1) {
+      console.log("Too high");
+    } else console.log("This was not a number");
+  });
+}
+module.exports = numberGame;
